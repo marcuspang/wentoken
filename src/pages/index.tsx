e909eacbe454bc8b0f4fcdc376cdce3f3d32be86
randomRadish <marcuspangyuyang@gmail.com>
2022-01-22 15:06:49 +0800

    Initial

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>wenToken</title>
+        <meta
+          name="description"
+          content="NFT Portfolio and trading platform for inter-NFT trades"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>Main</main>
+      <footer>Footer</footer>
+    </div>
+  );
+}

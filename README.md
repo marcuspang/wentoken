2cdede4db88d97b101a0cd8fca1e5bcf8f59c02e
randomRadish <marcuspangyuyang@gmail.com>
2022-01-23 03:47:51 +0800

    Add execute trade functions and add tests for smart contract

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 ## Contracts
 
-explore page: showing 4 types of token to purchase
-transfering from this account to yours
-selling from portfolio: store offer on our database, so other users when they click
-they will call the transfer function
-trade page: safeBatchTransfer for all transactions
-NFT <> NFT
+- explore page: showing 4 types of token to purchase, transfering from original account to buyer
+- selling from portfolio: store offers on our database, when other users purchase the transfer function is called
+- trade page: safeBatchTransfer for all NFT <> NFT transactions
--- contracts/WenToken.sol
+++ contracts/WenToken.sol
@@ -10,6 +10,8 @@ contract WenToken is ERC1155, Ownable, ERC1155Supply {
   uint256 public constant BLUE = 1;
   uint256 public constant GREEN = 2;
   uint256 public constant RED = 3;
+  // mint fee same for all types
+  uint256 public constant MINT_FEE = 0.01 ether;
 
   constructor() ERC1155("") {
     _mint(msg.sender, BLACK, 10**1, "");
@@ -41,7 +43,6 @@ contract WenToken is ERC1155, Ownable, ERC1155Supply {
   }
 
   // The following functions are overrides required by Solidity.
-
   function _beforeTokenTransfer(
     address operator,
     address from,
@@ -51,5 +52,55 @@ contract WenToken is ERC1155, Ownable, ERC1155Supply {
     bytes memory data
   ) internal override(ERC1155, ERC1155Supply) {
     super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+  }
+
+  function payToMint(
+    address to,
+    uint256 id,
+    uint256 amount,
+    bytes memory data
+  ) public payable {
+    require(msg.value >= amount * MINT_FEE, "Insufficient amount paid");
+    _mint(to, id, amount, data);
+  }
+
+  function payToMintBatch(
+    address to,
+    uint256[] memory ids,
+    uint256[] memory amounts,
+    bytes memory data
+  ) public payable {
+    uint256 sum = 0;
+    for (uint256 i = 0; i < ids.length; i++) {
+      sum += MINT_FEE * amounts[i];
+    }
+    require(msg.value >= sum, "Insufficient amount paid");
+    _mintBatch(to, ids, amounts, data);
+  }
+
+  function executeTrade(
+    address from,
+    address to,
+    uint256 fromId,
+    uint256 fromAmount,
+    uint256 toId,
+    uint256 toAmount,
+    bytes memory data
+  ) public {
+    safeTransferFrom(from, to, fromId, fromAmount, data);
+    safeTransferFrom(to, from, toId, toAmount, data);
+  }
+
+  function executeTradeBatch(
+    address from,
+    address to,
+    uint256[] memory fromIds,
+    uint256[] memory fromAmounts,
+    uint256[] memory toIds,
+    uint256[] memory toAmounts,
+    bytes memory data
+  ) public {
+    safeBatchTransferFrom(from, to, fromIds, fromAmounts, data);
+    safeBatchTransferFrom(to, from, toIds, toAmounts, data);
   }
 }
